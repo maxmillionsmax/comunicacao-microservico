e5c7957189f7974234b495d93a263e9365b200b8
maxmillionsmax <maximilianocaetano@gmail.com>
2022-09-16 02:07:56 -0300

    criação dos microserviços

---
+++ produtct-api/src/main/java/br/com/cursoudemy/produtctapi/ProdutctApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursoudemy.produtctapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutctApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutctApiApplication.class, args);
+	}
+
+}
---
+++ produtct-api/src/test/java/br/com/cursoudemy/produtctapi/ProdutctApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursoudemy.produtctapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutctApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
